3ee13d23dd86a5dd2fb844676d97bc9609a5a576
okbuddyhololive <okbuddyhololive@okbuddyholo.live>
2022-03-25 19:46:01 +0100

    feat: add systemd setup

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ wget -c https://the-eye.eu/public/AI/GPT-J-6B/step_383500_slim.tar.zstd
 tar -I zstd -xf step_383500_slim.tar.zstd
 sudo python3 -m pip uninstall tf-nightly tb-nightly -y
 python3 -m pip install jax[tpu] jaxlib -f https://storage.googleapis.com/jax-releases/jax_releases.html
-python3 -m pip install git+git@github.com:clashluke/mesh-transformer-jax
+python3 -m pip install git+git@github.com:okbuddyhololive/mesh-transformer-jax
 python3 -m pip install -r requirements.txt
---
+++ setup_systemd.sh
@@ -0,0 +1,4 @@
+sudo cp scripts/discord.service /etc/systemd/system/discord.service
+sudo systemctl daemon-reload
+sudo systemctl enable discord.service
+sudo systemctl start discord.service
