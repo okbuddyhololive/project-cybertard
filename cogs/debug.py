c06a3884af3e746b4cd7316b38ba3af9a53af8c3
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-03-27 14:47:20 +0200

    fix(debug): remove kwarg

--- cogs/debug.py
+++ cogs/debug.py
@@ -1,56 +1,46 @@
+import typing
 from typing import Optional
 
+import yaml
 from discord.ext import commands
 
 
 class Debug(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
-    
+
     async def _get_config(self, ctx, key: str):
         key = key.lower()
-        value = getattr(
-            self.bot.infer_config,
-            key,
-            default=None
-        )
-        
+        value = getattr(self.bot.infer_config, key, None)
+
         if value is None:
             return await ctx.send(f"Attribute `{key}` does not exist in the inference config")
-        
+
         await ctx.send(f"Attribute `{key}` is `{value}`")
-    
+
     async def _set_config(self, ctx, key: str, value):
         key = key.lower()
-        original_value = getattr(
-            self.bot.infer_config,
-            key,
-            default=None
-        )
-        
+        original_value = getattr(self.bot.infer_config, key, None)
+
         if original_value is None:
             return await ctx.send(f"Attribute `{key}` does not exist in the inference config")
-        
+
         # since type(x) returns a class for a variable type, 
         # you can call it to convert an another value
         value = type(original_value)(value)
-        
-        setattr(
-            self.bot.infer_config,
-            key,
-            value
-        )
-        
+
+        setattr(self.bot.infer_config, key, value)
+
         await ctx.send(f"Succesfully set `{key}` from `{original_value}` to `{value}`")
-        
+
     @commands.command()
     @commands.is_owner()
     async def config(self, ctx, key: str, *, value=None):
         if value is None:
             return await self._get_config(ctx, key)
-        
+
         await self._set_config(ctx, key, value)
-        
+
 
 def setup(bot):
     bot.add_cog(Debug(bot))
