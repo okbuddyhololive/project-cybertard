ddac60acb7e82670d83093f7fe0a9582c78213d9
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-03-27 20:28:34 +0200

    feat(chatbot): allow wrongly capitalized mentions of self

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -8,6 +8,11 @@ import discord
 
 from model import Inference, ModelParams
 
+_key = "[a-zA-Z0-9\.]+"
+_AUTO_MESSAGE = re.compile(f"(Attribute `[a-zA-Z0-9.]+(` is `{_key}`|` does not exist in the inference config)|"
+                           f"Successfully set `{_key}` from `{_key}` to `{_key}`|"
+                           f"config:\n    {key}: {key})")
+
 
 class Chatbot(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -24,9 +29,13 @@ class Chatbot(commands.Cog):
         if channel_id not in self.bot.config["channels"]:
             return
 
+        content = message.clean_content
+        if message.author == self.bot.user and _AUTO_MESSAGE.match(content):
+            return
+
         # adding message to the prompt
         # TODO: Extract split fn
-        self.prompt[channel_id] += f"<{message.author.name}>: {message.clean_content}\n-----\n"
+        self.prompt[channel_id] += f"<{message.author.name}>: {content}\n-----\n"
         prompt = self.prompt[channel_id]
 
         if len(prompt) > self.bot.infer_config.prompt_length:
@@ -45,7 +54,8 @@ class Chatbot(commands.Cog):
             function = functools.partial(self.model.generate, prompt=messages + f"<{self.bot.infer_config.name}>:")
             response = await self.bot.loop.run_in_executor(None, function)
             response = response.split("\n-----\n")[0]
-            response = response.replace(self.bot.infer_config.name, f'<@{self.bot.user.id}>')
+            response = re.sub('@' + ''.join(f'({c.lower()}|{c.upper()})' for c in self.bot.infer_config.name),
+                              f'<@{self.bot.user.id}>', response)
             # replacing names with actual mentions so that mentions actually work
             for user in self.bot.users:
                 response = response.replace(f"@{user.name}", f"<@{user.id}>")
