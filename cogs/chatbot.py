301a7f3edbf4fe7a0d885012ca0247f30952ba8b
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-03-27 14:37:40 +0200

    fix(chatbot): move code back again

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -12,10 +12,7 @@ from model import Inference, ModelParams
 class Chatbot(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.model = Inference(
-            parameters=ModelParams(**self.bot.config["Model"]),
-            config=self.bot.infer_config,
-        )
+        self.model = Inference(parameters=ModelParams(**self.bot.config["Model"]), config=self.bot.infer_config, )
         self.prompt = collections.defaultdict(str)
 
     @commands.Cog.listener()
@@ -41,18 +38,16 @@ class Chatbot(commands.Cog):
         if self.bot.user not in message.mentions and random.random() >= self.bot.infer_config.response_probability:
             return
 
-        # replacing names with actual mentions so that mentions actually work
-        for user in self.bot.users:
-            response = response.replace(f"@{user.name}", f"<@{user.id}>")
-        
         messages = prompt.replace(self.bot.user.name, self.bot.infer_config.name)
-        
-        # partial function needed for async
-        function = functools.partial(self.model.generate, prompt=messages + f"<{self.name}>:")
 
         async with message.channel.typing():
+            # partial function needed for async
+            function = functools.partial(self.model.generate, prompt=messages + f"<{self.name}>:")
             response = await self.bot.loop.run_in_executor(None, function)
             response = response.split("\n-----\n")[0]
+            # replacing names with actual mentions so that mentions actually work
+            for user in self.bot.users:
+                response = response.replace(f"@{user.name}", f"<@{user.id}>")
 
         if response:
             await message.reply(response, mention_author=False)
