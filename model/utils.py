e4e4ccfae944f831c1825b01e0d6a6f59e8990e5
Lucas Nestler <39779310+ClashLuke@users.noreply.github.com>
2022-03-29 19:23:26 +0200

    fix(models/utils): replace invalid emoji ids

--- model/utils.py
+++ model/utils.py
@@ -7,7 +7,7 @@ _key = "[a-zA-Z0-9\.]+"
 _AUTO_MESSAGE = re.compile(f"(Attribute `[a-zA-Z0-9.]+(` is `{_key}`|` does not exist in the inference config)|"
                            f"Successfully set `{_key}` from `{_key}` to `{_key}`|"
                            f"config:\n    {_key}: {_key})")
-
+_EMOJI_RE = re.compile(f"<(:{_key}:)[0-9]+>")
 
 def replace_ping(original_name: str, new_id: int, text: str) -> str:
     return re.sub('@' + ''.join(f'({c.lower()}|{c.upper()})' for c in original_name), f'<@{new_id}>', text)
@@ -21,6 +21,8 @@ def fix_emojis_and_mentions(text: str, users: List[User], emojis: List[Emoji]) -
     for user in users:
         #text = replace_ping(user.name, user.id, text)
         text = text.replace(f"@{user.name}", f"<@{user.id}>")
+
+    text = _EMOJI_RE.replace("\\g<1>", text)
 
     for emoji in emojis:
         if f"<:{emoji.name}:{emoji.id}>" in text:
