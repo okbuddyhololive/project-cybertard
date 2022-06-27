417f5e66902a86e2ebc2e19022000e51911350fd
Lucas Nestler <39779310+ClashLuke@users.noreply.github.com>
2022-06-27 10:15:22 +0200

    fix(setup): install from new TPU url

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ sudo apt install zstd
 wget -c https://the-eye.eu/public/AI/GPT-J-6B/step_383500_slim.tar.zstd
 tar -I zstd -xf step_383500_slim.tar.zstd
 sudo python3 -m pip uninstall tf-nightly tb-nightly -y
-python3 -m pip install jax[tpu] jaxlib -f https://storage.googleapis.com/jax-releases/jax_releases.html
+python3 -m pip install jax[tpu] jaxlib -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 python3 -m pip install git+https://github.com/okbuddyhololive/mesh-transformer-jax
 python3 -m pip install -r requirements.txt
 wget https://raw.githubusercontent.com/okbuddyhololive/mesh-transformer-jax/master/requirements.txt -O tmp.txt
